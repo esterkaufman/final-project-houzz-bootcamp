3f6c8112a15271dc04a7a06e518fd6bda68d7d3e
Ester Kaufman <ester.kaufman@sap.com>
2022-08-30 16:39:37 +0300

    chat bot page

---
+++ client/src/items/componnent/support-module/chatbot.tsx
@@ -0,0 +1,25 @@
+import { Component, ReactNode } from "react";
+import WithRouter, { IWithRouterProps } from "../../app/with-router";
+
+
+
+class ChatBotPage extends Component {
+
+  constructor(props) {
+    super(props);
+   
+  }
+
+  render() : ReactNode{
+   
+    return (
+      <div>       
+      </div>
+    );
+  }
+
+}
+export default WithRouter(ChatBotPage);
+
+
+
