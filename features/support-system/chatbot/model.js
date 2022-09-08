355cf254e7d8893c273accef58228b05487a6bfb
Zislw <w05567716@gmail.com>
2022-09-08 12:38:59 +0300

    first commit 08/09/2022

---
+++ features/support-system/chatbot/model.js
@@ -0,0 +1,20 @@
+import {createMongooseModel} from "../../../app/API/create-quick/mongo"
+
+const chatbot=createMongooseModel("chatbot",{
+    titel:{
+        type:String,
+        require :[true,"you must choose titel"]
+    },
+    solution:{
+            type:[String],
+            }
+        },
+    )
+    export default chatbot
+    
+    
+    
+    
+    
+    
+    
