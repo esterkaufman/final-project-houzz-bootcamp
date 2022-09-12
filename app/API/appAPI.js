e7468719e6ad862ddf61855e944fc81b6c4dd604
ayalachadash <ayala0533121955@gmail.com>
2022-09-12 14:41:24 +0300

    product API

--- app/API/appAPI.js
+++ app/API/appAPI.js
@@ -1,5 +1,7 @@
 import basicAppWithExpress from "./create-quick/express.js";
 import userRrouter from "../users/controllers.js";
+import productRouter from "../product/controllers.js";
+import cartRouter from "../cart/controllers.js"
 import signRouter from "../../features/admin/sign/controllers.js";
 import errHendler from "./authentications-and-authorizations/error-handler.js";
 
@@ -7,6 +9,8 @@ const appAPI = () => {
   const app = basicAppWithExpress();
   app.use(signRouter);
   app.use("/users", userRrouter);
+  app.use("/products", productRouter);
+  app.use("/cart", cartRouter);
   app.use(errHendler);
 };
 
