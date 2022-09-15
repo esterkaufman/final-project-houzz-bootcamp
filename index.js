b8d8eff9061b2664b0c15db07f3a162d7199105e
Ester Kaufman <ester.kaufman@sap.com>
2022-09-15 16:58:38 +0300

    fix merge

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-import CMomgoDB from "./server/CQ-API/mongo.js";
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import userRrouter from "./server/users/controllers.js";
-import conectionRouter from "./server/conection/controllers.js";
-import errHendler from "./server/conection/error-handler.js";
-=======
+
 import appAPI from "./app/API/appAPI.js"
 import mongoDBConnection from './app/API/create-quick/mongo.js'
->>>>>>> origin/master
 
 appAPI()
 mongoDBConnection()
